e8c2c7521a001d0b67c67b835d1c0a5b525dd2f6
Juan Manuel Depaolo <116757984+DepaoloJuan@users.noreply.github.com>
2024-10-13 12:16:54 -0300

    Add files via upload

---
+++ RTA_SCRIPT_Examen_20241013/Punto_D.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+mkdir -p Estructura_Asimetrica/correo Estructura_Asimetrica/clientes
+
+
+touch Estructura_Asimetrica/correo/cartas_{1..100}
+
+
+touch Estructura_Asimetrica/clientes/cartas_{1..100}
+
+
+touch Estructura_Asimetrica/correo/carteros_{1..10}
+
